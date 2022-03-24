c351de3c8bde2ce7a47d4a0d994af102eaeb7d03
Griffin Lundquist <griffin@arcadiaitpartners.com>
2022-03-24 17:55:43 -0500

    file restructer

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const alpha = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*-_=+"; 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
